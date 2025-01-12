9341ae4fff08ef2f54930a2ae3a094a07ab77899
Daniel Lin <dannylin1765@gmail.com>
2025-01-11 17:00:16 -0800

    Test update to test github action
    Add new workflow to test branch should deploy a preview to vercel

--- src/components/ProductionReport.tsx
+++ src/components/ProductionReport.tsx
@@ -275,7 +275,7 @@ export function ProductionReport() {
                		<CardTitle>
                         <div className = "flex justify-between items-center">
                            <span className = "text-xl font-bold">
-                              {device} Production Report
+                              {device} Production Report I AM EDITED
                            </span>
                            <span className = "text-sm text-muted-foreground">
                               {format(startDate, "MMM d, yyyy")} - {" "}
